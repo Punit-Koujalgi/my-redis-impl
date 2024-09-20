d64186e503bf8500c9447ad4244b32ca2d11f491
Punit-Koujalgi <punit.utilmore@gmail.com>
2024-09-20 11:25:45 +0530

    Reading from rdb file: get command 2

--- src/KeyValueStore.cpp
+++ src/KeyValueStore.cpp
@@ -185,9 +185,12 @@ void KeyValueStore::initializeKeyValues(const std::string& dbpath, const std::st
 			std::cout << "Adding " << key << " -> " << value << std::endl;
 			m_mapKeyValues[key] = value;
 
-			t.tv_sec = expire_time_s;
-			t.tv_usec = expire_time_ms / 1000;
-			m_mapKeyTimeouts[key] = t;
+			if (expire_time_s != 0)
+			{
+				t.tv_sec = expire_time_s;
+				t.tv_usec = expire_time_ms / 1000;
+				m_mapKeyTimeouts[key] = t;
+			}
 		}
 
 	}
